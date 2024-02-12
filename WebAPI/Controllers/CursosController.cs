d7e072bf04a6d6906710ad43cde15a9fcbaf41e7
Jose <david-restrepo_123@hotmail.es>
2024-02-12 08:29:03 -0500

    Creating new courses and creating the controller for the new courses

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -33,5 +33,11 @@ namespace WebAPI.Controllers
         {
             return await _mediator.Send(new ConsultaId.CursoUnico{Id = id});
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Nuevo.Ejecuta data)
+        {
+            return await _mediator.Send(data);
+        }
     }
 }
